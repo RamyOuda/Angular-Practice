16ff6b856e1897cfa952f5ef2fa7a30ecf84a852
Ramy Ouda <ramyouda@gmail.com>
2022-10-13 09:37:48 -0400

    Create 2 methods to show/hide message popup, create getter

--- routing/src/app/app.component.ts
+++ routing/src/app/app.component.ts
@@ -8,6 +8,7 @@ import {
   Event,
 } from '@angular/router';
 import { slideInAnimation } from './app.animation';
+import { MessageService } from './messages/message.service';
 
 import { AuthService } from './user/auth.service';
 
@@ -32,7 +33,15 @@ export class AppComponent {
     return '';
   }
 
-  constructor(private authService: AuthService, private router: Router) {
+  get isMessageDisplayed(): boolean {
+    return this.messageService.isDisplayed;
+  }
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private messageService: MessageService
+  ) {
     router.events.subscribe((routerEvent: Event) => {
       this.checkRouterEvent(routerEvent);
     });
@@ -57,5 +66,15 @@ export class AppComponent {
     console.log('Log out');
 
     this.router.navigateByUrl('/welcome');
+  }
+
+  displayMessages(): void {
+    this.router.navigate([{ outlets: { popup: ['messages'] } }]);
+    this.messageService.isDisplayed = true;
+  }
+
+  hideMessages(): void {
+    this.router.navigate([{ outlets: { popup: null } }]);
+    this.messageService.isDisplayed = false;
   }
 }
