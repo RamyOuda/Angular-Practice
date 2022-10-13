6209b55065eae5af7777212ebe5ec675010e5122
Ramy Ouda <ramyouda@gmail.com>
2022-10-13 12:06:09 -0400

    Modify Generic Property

--- routing/src/app/products/product-edit/preduct-edit.guard.ts
+++ routing/src/app/products/product-edit/preduct-edit.guard.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanDeactivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import { ProductEditComponent } from './product-edit.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class PreductEditGuard implements CanDeactivate<unknown> {
+export class PreductEditGuard implements CanDeactivate<ProductEditComponent> {
   canDeactivate(
-    component: unknown,
+    component: ProductEditComponent,
     currentRoute: ActivatedRouteSnapshot,
     currentState: RouterStateSnapshot,
-    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    nextState?: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
     return true;
   }
-  
 }
