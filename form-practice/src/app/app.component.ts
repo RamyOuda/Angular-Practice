79c2a0090218aba0f1ca28049746c618b3ed115d
Ramy Ouda <ramyouda@gmail.com>
2022-10-15 21:58:59 -0400

    Initial Commit

---
+++ form-practice/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  practiceForm!: FormGroup;
+
+  ngOnInit() {
+    this.practiceForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      address: new FormControl(),
+    });
+  }
+}
